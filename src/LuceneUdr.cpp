8084c2a8589de8a6dc1d545915d4a2bc3f938a04
Simonov Denis <sim-mail@list.ru>
2024-07-12 12:36:07 +0300

    .

---
+++ src/LuceneUdr.cpp
@@ -0,0 +1,16 @@
+/**
+ *  Lucene full-text index file helper.
+ *
+ *  The original code was created by Simonov Denis
+ *  for the open source project "IBSurgeon Full Text Search UDR".
+ *
+ *  Copyright (c) 2022 Simonov Denis <sim-mail@list.ru>
+ *  and all contributors signed below.
+ *
+ *  All Rights Reserved.
+ *  Contributor(s): ______________________________________.
+ **/
+
+#include "LuceneUdr.h"
+
+FB_UDR_IMPLEMENT_ENTRY_POINT
